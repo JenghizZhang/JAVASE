49e488ce74bd1b04ef6047650c9a94682f824c8a
Yunsong Zhang <yunsong@gatech.edu>
2022-03-19 23:18:02 -0400

    HotKey

---
+++ module01_array/src/com/atguigu/javase/idea/HotKetTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.javase.idea;
+import java.util.Scanner;
+
+public class HotKetTest {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        int nextInt = scanner.nextInt();
+        System.out.println(nextInt);
+    }
+}
