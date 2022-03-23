b7963ab7ca58228164285f41ed0bad62db6382ca
Yunsong Zhang <yunsong@gatech.edu>
2022-03-23 19:19:56 -0400

    Method

--- Module03_class/src/com/atguigu/object/test/Calculator.java
+++ Module03_class/src/com/atguigu/object/test/Calculator.java
@@ -1,10 +1,55 @@
 package com.atguigu.object.test;
 
 public class Calculator {
-    public double add(double a, double b){
+    // 这个方法针对double类型
+    public double add(double a, double b) {
         return a + b;
     }
-    public int add(int a, int b){
+    
+    // 这个方法针对int类型
+    public int add(int a, int b) {
         return a + b;
+    }
+    
+    // 三个input
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    
+    // e可变参数
+    public int add(int... num) {
+        int sum = 0;
+        for (int j : num) {
+            sum += j;
+        }
+        return sum;
+    }
+    
+    public int mOL(int i) {
+        return (int) Math.pow(i, 2);
+    }
+    
+    public int mOL(int i, int j) {
+        return i * j;
+    }
+    
+    public void mOL(String value) {
+        System.out.println("value = " + value);
+    }
+    
+    public int recursiveAdd(int num) {
+        if (num == 1) {
+            return 1;
+        } else {
+            return recursiveAdd(num - 1) + num;
+        }
+    }
+    
+    public int recursiveMul(int num){
+        if(num ==2){
+            return 2;
+        }else{
+            return num * recursiveMul(num - 1);
+        }
     }
 }
--- Module03_class/src/com/atguigu/object/test/CalculatorTest.java
+++ Module03_class/src/com/atguigu/object/test/CalculatorTest.java
@@ -2,6 +2,18 @@ package com.atguigu.object.test;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        System.out.println(new Calculator().add(1,2.0 ));
+        System.out.println(new Calculator().add(1, 2.0));
+        
+        Calculator cal = new Calculator();
+        int r1 = cal.mOL(5);
+        System.out.println("r1 = " + r1);
+        int r2 = cal.mOL(3, 6);
+        System.out.println("r2 = " + r2);
+    
+        cal.mOL("123123123");
+        System.out.println(new Calculator().add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        System.out.println(new Calculator().recursiveAdd(10));
+        System.out.println(new Calculator().recursiveMul(5));
+        
     }
 }
