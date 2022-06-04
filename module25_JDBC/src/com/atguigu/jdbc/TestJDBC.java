6ca1652c096b1d97c271b2469f9ecaffc5e763ea
Yunsong Zhang <yunsong@gatech.edu>
2022-06-03 23:52:51 -0400

    JDBC update and query

---
+++ module25_JDBC/src/com/atguigu/jdbc/TestJDBC.java
@@ -0,0 +1,89 @@
+package com.atguigu.jdbc;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class TestJDBC {
+    private Connection connection;
+    private Statement statement;
+    
+    @Before
+    public void init() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql:///atguigu?characterEncoding=utf8&serverTimezone=GMT";
+        String user = "root";
+        String password = "199834zys";
+        connection = DriverManager.getConnection(url, user, password);
+        statement = connection.createStatement();
+    }
+    
+    @After
+    public void destroy() throws Exception {
+        statement.close();
+        connection.close();
+    }
+    
+    @Test
+    public void testFindAll() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql:///atguigu?characterEncoding=utf8&serverTimezone=GMT";
+        String user = "root";
+        String password = "199834zys";
+        Connection connection = DriverManager.getConnection(url, user, password);
+        Statement statement = connection.createStatement();
+        String sql = "select * from user";
+        ResultSet resultSet = statement.executeQuery(sql);
+        
+        while (resultSet.next()) {
+            int id = (int) resultSet.getObject("id");
+            String un = (String) resultSet.getObject("username");
+            String pwd = (String) resultSet.getObject("password");
+            String nn = (String) resultSet.getObject("nickname");
+            System.out.println(id + un + pwd + nn);
+            System.out.println("!!!!!!!!");
+        }
+        
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+    
+    @Test
+    public void testInsert() throws Exception {
+        String sql = "insert into user (username,password,nickname) values ('tq','7777','天气')";
+        int i = statement.executeUpdate(sql);
+        System.out.println(i);
+    }
+    
+
+    @Test
+    public void testDelete() throws Exception {
+        String sql = "delete from user where id>=4";
+        int i = statement.executeUpdate(sql);
+        System.out.println("i = " + i);
+    }
+    
+    @Test
+    public void testUpdate() throws Exception {
+        String sql = "update user set nickname = '王武' where id=  3";
+        int i = statement.executeUpdate(sql);
+        System.out.println("i = " + i);
+    }
+    
+    @Test
+    public void testFindById() throws Exception {
+        String sql = "select * from user where id = 1";
+        ResultSet resultSet = statement.executeQuery(sql);
+        while (resultSet.next()){
+            System.out.println(resultSet.getObject(2));
+        }
+        resultSet.close();
+    }
+    
+}
